e9ef81852168e968dbdb6068ad9b99534b8e3be8
Kamil Trzmiel <k.trzmiel@gmail.com>
2023-08-09 21:59:06 +0200

    done

--- contacts.js
+++ contacts.js
@@ -1,20 +1,46 @@
-const fs = require('fs/promises');
-const path = require('path');
-const { nanoid } = require('nanoid');
+import fs from 'fs/promises';
+import path from 'path';
+import { nanoid } from 'nanoid';
 
-//
-function listContacts() {
-  // ...
-}
+export const contactPath = path.resolve('db', 'contacts.json');
 
-function getContactById(contactId) {
-  // ...
-}
+//pobranie aktualnej listy kontaktów
+export const listContacts = async () => {
+  const response = await fs.readFile(contactPath);
+  return JSON.parse(response);
+};
 
-function removeContact(contactId) {
-  // ...
-}
+//pobranie kontaktu z podanym contactId
+export const getContactById = async contactId => {
+  const fullList = await listContacts();
+  const response = fullList.find(contact => contact.id === contactId);
+  return response || null;
+};
 
-function addContact(name, email, phone) {
-  // ...
-}
+//usuwanie kontaktu z podanym contactId
+export const removeContact = async contactId => {
+  const fullList = await listContacts();
+  const index = fullList.findIndex(contact => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [response] = fullList.splice(index, 1);
+  await fs.writeFile(contactPath, JSON.stringify(fullList, null, 2));
+  return response;
+};
+
+//dodawanie kontaktu
+export const addContact = async data => {
+  const fullList = await listContacts();
+  const newContact = { id: nanoid(), ...data };
+  fullList.push(newContact);
+  await fs.writeFile(contactPath, JSON.stringify(fullList, null, 2));
+  return newContact;
+};
+
+export default {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,22 +1,51 @@
-const contacts = require('./contacts');
-const { commands } = require('commander');
+import { Command } from 'commander';
+import os from 'os';
+import { listContacts, getContactById, removeContact, addContact } from './contacts.js';
+
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case 'list':
-      // ...
+      const fullList = await listContacts();
+      console.log(
+        '\n' + new Date() + '\n' + os.type + '\n*************** FULL CONTACT LIST ***************'
+      );
+      return console.table(fullList);
       break;
 
     case 'get':
-      // ... id
+      const contactById = await getContactById(id);
+      console.log(
+        '\n' + new Date() + '\n' + os.type + '\n*************** CONTACT BY ID ***************'
+      );
+      return console.table(contactById);
       break;
 
     case 'add':
-      // ... name email phone
+      const newContact = await addContact({ name, email, phone });
+      console.log(
+        '\n' + new Date() + '\n' + os.type + '\n*************** CONTACT ADDED ***************'
+      );
+      return console.table(newContact);
       break;
 
     case 'remove':
-      // ... id
+      const deleteContact = await removeContact(id);
+      console.log(
+        '\n' + new Date() + '\n' + os.type + '\n*************** CONTACT REMOVED ***************'
+      );
+      return console.table(deleteContact);
       break;
 
     default:
@@ -24,14 +53,4 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
   }
 };
 
-commands
-  .option('-a, --action <type>', 'choose action')
-  .option('-i, --id <type>', 'user id')
-  .option('-n, --name <type>', 'user name')
-  .option('-e, --email <type>', 'user email')
-  .option('-p, --phone <type>', 'user phone');
-
-commands.parse();
-
-const option = commands.opts();
 invokeAction(argv);
